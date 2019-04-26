57ab43f309420160a52df6052532ab6c65cb380d
aidanaarts <aidanaarts@gmail.com>
2019-04-26 11:29:53 -0600

    Create Trie.hpp

---
+++ Trie.hpp
@@ -0,0 +1,63 @@
+#ifndef TRIE_HPP
+#define TRIE_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Playlist.hpp"
+using namespace std;
+/*
+struct songNode
+{
+    string name;
+    string artist;
+    string genre;
+    int length[2];
+    songNode* next;
+    float prioritynum;
+    songNode()
+    {
+      length[0] = 0;
+      length[1] = 0;
+      next = NULL;
+      prioritynum = 0;
+    }
+};
+*/
+struct node
+{
+    char name;
+    node *children[26];
+    node* parent;
+    songNode* head;
+    bool isempty;
+    node()
+    {
+      parent = NULL;
+      head = NULL;
+      isempty = true;
+      for (int i = 0; i < 26; i++)
+      {
+        children[i] = NULL;
+      }
+    }
+};
+
+class Trie
+{
+    public:
+        Trie();
+        ~Trie();
+        void addSong(songNode *song);
+        //void deleteSong(songNode *song);
+        songNode* findSong(string name, string star);
+        char getChar(int num);
+        void printTrie();
+        void printTop5(string search);
+        node* gethead();
+    private:
+        node* head;
+};
+
+#endif
